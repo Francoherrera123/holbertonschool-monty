5a1695b5a1c213cc354371dad58584ba79b84711
root <Francoherrera123>
2023-05-14 09:46:11 -0700

    More opcode update

--- more_opcode_functions.c
+++ more_opcode_functions.c
@@ -24,4 +24,14 @@ void _pop(stack_t **stack, unsigned int line_number)
 		*stack = NULL;
 	free(stack_tmp);
 }
+/**
+ * _nop - doesn't do anything
+ * @stack: pointer to stack_t
+ * @line_number: number of line
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
 
