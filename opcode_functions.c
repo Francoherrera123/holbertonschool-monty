25359cea4cc2db5c73f7dcdc259d27edc942f6b2
root <Francoherrera123>
2023-05-14 09:36:02 -0700

    opcode update

--- opcode_functions.c
+++ opcode_functions.c
@@ -59,7 +59,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		fclose(file);
 		exit(EXIT_FAILURE);
 	}
