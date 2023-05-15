0b99a490f03e456e7ac1c5f7f72afc3fb42bad05
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-05-14 23:06:06 -0700

    addandswap

--- opcode_functions.c
+++ opcode_functions.c
@@ -66,9 +66,44 @@ void _pint(stack_t **stack, unsigned int line_number)
 	printf("%d\n", (*stack)->n);
 }
 
+/**
+* _swap - swaps the top two elements of the stack
+* @stack: pointer to a stack_t
+* @line_number: number of line
+*/
+void _swap(stack_t **stack, unsigned int line_number)
+{
+int tmp;
 
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
 
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
 
+/**
+* _add - adds the top two elements of the stack
+* @stack: pointer to a stack_t
+* @line_number: number of line
+*/
+void _add(stack_t **stack, unsigned int line_number)
+{
+int sum;
 
+if (!(*stack) || !(*stack)->next)
+{
+	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+	fclose(file);
+	exit(EXIT_FAILURE);
+}
 
-
+sum = (*stack)->n + (*stack)->next->n;
+_pop(stack, line_number);
+(*stack)->n = sum;
+}
